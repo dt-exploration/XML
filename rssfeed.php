f2be399d9c42a5bbda77a9f571e5f11d7cacad81
Dusan <masinacdt@gmail.com>
2019-02-11 23:04:16 +0100

    appended child

--- rssfeed.php
+++ rssfeed.php
@@ -5,14 +5,31 @@
 
 $doc=new DOMDocument();
 
-
 $rss=$doc->createElement('rss');
 $channel=$doc->createElement('channel');
-$doc->createElement('item');
-$doc->createElement('title');
-$doc->createElement('link');
-$doc->createElement('description');
-header('content type: text/xml')
+
+$c_title->createElement('title');
+$c_link->createElement('link');
+$c_description->createElement('description');
+$c_language->createElement('language');
+
+$item->createElement('item');
+$i_title->createElement('title');
+$i_link->createElement('link');
+$i_description->createElement('description');
+
+$doc->appendChild($rss);
+$rss->appendChild($channel);
+
+$channel->appendChild($c_title);
+$channel->appendChild($c_link);
+$channel->appendChild($c_description);
+$channel->appendChild($c_language);
+
+header('content type: text/xml');
+$et=$doc->saveXML();
+
+echo $et;
 
 
 
