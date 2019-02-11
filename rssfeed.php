0a2348a06b04ba453d41b520209c301b85e54c72
Dusan <masinacdt@gmail.com>
2019-02-11 22:55:01 +0100

    rss feed start

---
+++ rssfeed.php
@@ -0,0 +1,22 @@
+<?php
+
+<?php
+//Creating rss feed
+
+$rss=new DOMDocument();
+
+
+$doc->createElement('rss');
+$doc->createElement('channel');
+$doc->createElement('rss');
+$doc->createElement('channel');$doc->createElement('rss');
+$doc->createElement('channel');$doc->createElement('rss');
+$doc->createElement('channel');
+header('content type: text/xml')
+
+
+
+
+
+
+?>
